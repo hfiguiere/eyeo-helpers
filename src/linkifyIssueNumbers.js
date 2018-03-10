f6a4d146ba73da85a4434947f339c735794e3336
Sebastian Noack <sebastian.noack@gmail.com>
2018-03-09 21:48:09 -0500

    Close XSS vulnerability when transforming review titles

--- src/linkifyIssueNumbers.js
+++ src/linkifyIssueNumbers.js
@@ -7,16 +7,13 @@
   // Find sections with issue IDs and then linkify each issue ID.
   let sectionRe = /#\d+|Issues?\s+#?(?:\d+.\s*)+/gi;
   let header = document.querySelector("body div h2");
-  let decoratedNode = document.createElement("span");
-  decoratedNode.innerHTML = header.removeChild(header.lastChild).textContent
-    .replace(
-      sectionRe,
-      matchingString => matchingString.replace(
-        /\d+/g,
-        issueID => "<a href=\"https://issues.adblockplus.org/ticket/" +
-                   issueID + "\">" + issueID + "</a>"
-      )
-    );
-  header.appendChild(decoratedNode);
+  header.innerHTML = header.innerHTML.replace(
+    sectionRe,
+    matchingString => matchingString.replace(
+      /\d+/g,
+      issueID => "<a href=\"https://issues.adblockplus.org/ticket/" +
+                 issueID + "\">" + issueID + "</a>"
+    )
+  );
 }());
 
